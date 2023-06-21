d31d4ba49f4122ba443cf1bde7d621d5a7f2a541
MillionDream <milliondream@qq.com>
2023-06-21 12:22:36 +0800

    ✨ feat: Create Spread Vue2 Template.

--- Designer/PureJS/src/main.js
+++
@@ -1,9 +0,0 @@
-window.onload = function () {
-  let designer = new GC.Spread.Sheets.Designer.Designer(
-    document.getElementById("gc-designer-container")
-  );
-
-  let spread = designer.getWorkbook();
-
-  spread.fromJSON(json);
-};
--- Designer/Vue3/src/main.js
+++
@@ -1,6 +0,0 @@
-import { createApp } from 'vue'
-import App from './App.vue'
-import Designer from "@grapecity/spread-sheets-designer-vue"
-let app = createApp(App);
-app.component("gc-spread-sheets-designer", Designer);
-app.mount('#app')
