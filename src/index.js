5e6d7e8043ecc9fc54387c3bd9d4d636bf7b4b97
MillionDream <milliondream@qq.com>
2023-12-04 11:29:07 +0800

    🎉 init: Create JS_Gantt_Template

--- src/index.js
+++ src/index.js
@@ -1,29 +1,46 @@
 window.onload = function () {
   let spread = new GC.Spread.Sheets.Workbook(document.getElementById("ss"));
 
-  let sheet = spread.getActiveSheet();
-
-  let colInfos = [
-    { name: "order_num", displayName: "订单编号", width: 100 },
-    { name: "order_date", displayName: "订购日期", width: 150 },
-    { name: "type_name", displayName: "类别名称" },
-    { name: "product_name", displayName: "产品名称" },
-    { name: "quantity", displayName: "购买数量" },
-    { name: "unit_price", displayName: "产品单价" },
-    { name: "cost", displayName: "产品成本" },
-    { name: "discount", displayName: "折扣" },
-    { name: "order_amount", displayName: "订单金额" },
-    { name: "order_profit", displayName: "订单利润" },
-    { name: "sales_area", displayName: "销售大区" },
-    { name: "province", displayName: "销售省份" },
-    { name: "city", displayName: "销售城市" },
-    { name: "store", displayName: "销售门店" },
-    { name: "consultant", displayName: "销售顾问" },
-    { name: "pay_method", displayName: "支付方式" },
-    { name: "cus_name", displayName: "顾客姓名" },
-    { name: "cus_phone", displayName: "顾客电话" },
-  ];
-  sheet.autoGenerateColumns = false;
-  sheet.setDataSource(ds.data);
-  sheet.bindColumns(colInfos);
+  let dataManager = spread.dataManager();
+  // let apiUrl =
+  //   "https://demo.grapecity.com.cn/spreadjs/SpreadJSTutorial/server/api/Gantt_Id";
+  myTable = dataManager.addTable("myTable", {
+    batch: true,
+    data: ds,
+    // remote: {
+    //   read: {
+    //     url: apiUrl,
+    //   },
+    //   batch: {
+    //     url: apiUrl,
+    //   },
+    // },
+    schema: {
+      hierarchy: {
+        type: "Parent",
+        column: "parentId",
+      },
+      columns: {
+        id: { isPrimaryKey: true },
+        taskNumber: { dataType: "rowOrder" },
+      },
+    },
+  });
+  let ganttSheet = spread.addSheetTab(
+    0,
+    "GanttSheet",
+    GC.Spread.Sheets.SheetType.ganttSheet
+  );
+  let view = myTable.addView("ganttView", [
+    { value: "taskNumber", caption: "Task Number", width: 100 },
+    { value: "mode", caption: "Mode", width: 65 },
+    { value: "name", caption: "Task Name", width: 280 },
+    { value: "duration", caption: "Duration", width: 90 },
+    { value: "start", caption: "Start", width: 120 },
+    { value: "finish", caption: "Finish", width: 120 },
+    { value: "predecessors", caption: "Predecessors", width: 120 },
+  ]);
+  view.fetch().then(function () {
+    ganttSheet.bindGanttView(view);
+  });
 };
