779f8ef4db386e4ce2c26f770085525426ea42ad
MillionDream <milliondream@qq.com>
2023-12-05 12:24:34 +0800

    ✨ feat: Create Vue Pivot Template.

--- src/router/index.js
+++ src/router/index.js
@@ -1,34 +1,15 @@
 import { createRouter, createWebHashHistory } from "vue-router";
-import QuickStart from "@/components/QuickStart.vue";
-import DataBinding from "@/components/DataBinding.vue";
-import SpreadStyle from "@/components/SpreadStyle.vue";
-import OutLine from "@/components/OutLine.vue";
-
+import PivotTable from "../components/PivotTable.vue"
 let routes = [
   {
     path: "/",
-    redirect: "/QuickStart",
+    redirect: "/PivotTable",
   },
   {
-    path: "/QuickStart",
-    name: "QuickStart",
-    component: QuickStart,
-  },
-  {
-    path: "/DataBinding",
-    name: "DataBinding",
-    component: DataBinding,
-  },
-  {
-    path: "/SpreadStyle",
-    name: "SpreadStyle",
-    component: SpreadStyle,
-  },
-  {
-    path: "/OutLine",
-    name: "OutLine",
-    component: OutLine,
-  },
+    path: "/PivotTable",
+    name: "PivotTable",
+    component: PivotTable,
+  }
 ];
 
 export default createRouter({
